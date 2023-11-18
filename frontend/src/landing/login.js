484186b8b06ce82050b758cf9e7111e45b1fcbd9
tanmay-sketch <grandhi1@msu.edu>
2023-11-18 15:01:32 -0500

    Modified login.js

--- frontend/src/landing/login.js
+++ frontend/src/landing/login.js
@@ -1,7 +1,44 @@
+import React, { useState } from 'react';
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // basic validation
+    if (!username || !password) {
+      alert('Please enter both username and password.');
+      return;
+    }
+
+    console.log('Success');
+  }
+    // Simulate authentication logic
+
   return (
     <div className="Login">
-        Login
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
